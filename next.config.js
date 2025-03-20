fa15d56134470d6799af9078f34e245ed3a5c007
poiupmmm <>
2025-03-20 22:43:25 +0800

    优化构建配置和环境变量设置

--- next.config.js
+++ next.config.js
@@ -18,6 +18,15 @@ const nextConfig = {
     // !! 仅用于演示，生产环境不推荐
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
